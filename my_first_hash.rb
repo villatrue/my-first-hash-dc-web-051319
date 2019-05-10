e1acb64b510346aa9954c735948d93a31ca24e44
Sean Carter <scarter0313@gmail.com>
2019-05-10 21:08:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,6 +20,8 @@ def retrieval
                   "oil paintings" => 3
                  }
   shipping_manifest["whale bone corsets"]
+  shipping_manifest["porcelain vases"]
+  shipping_manifest["oil paintings"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
