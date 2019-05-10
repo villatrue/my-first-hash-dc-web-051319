9a7e88081eca928ed513c0d3f0c1f713bab8a1d9
Sean Carter <scarter0313@gmail.com>
2019-05-10 21:08:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,6 +19,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+  shipping_manifest["whale bone corsets"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
